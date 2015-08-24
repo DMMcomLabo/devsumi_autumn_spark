9ab19ab2645322968aebf9d684644d566c050700
root <root@srv4787.dmm.co.jp>
2015-08-24 11:50:04 +0900

    modified: implement read data from kafka , write data to hbase and write back to kafka

--- Build.sbt
+++ Build.sbt
@@ -29,6 +29,9 @@ libraryDependencies += "org.apache.hbase" % "hbase-protocol" % "1.0.0"
 
 libraryDependencies += "org.apache.htrace" % "htrace-core" % "3.0.4"
 
+libraryDependencies += "org.apache.kafka" % "kafka_2.10" % "0.8.2.1"
+
+
 assemblyMergeStrategy in assembly := {
   case PathList("javax", "servlet", xs @ _*)         => MergeStrategy.first
   case PathList(ps @ _*) if ps.last endsWith ".properties" => MergeStrategy.first
--- src/main/scala/TransposeActivityData.scala
+++ src/main/scala/TransposeActivityData.scala
@@ -1,12 +1,10 @@
 
-package com.dmm.i3.summary
+package com.dmm.i3.logging
 
 import java.util.Properties
 import kafka.javaapi.producer.Producer
 import kafka.producer.KeyedMessage
 import kafka.producer.ProducerConfig
-import kafka.serializer.StringDecoder
-import kafka.serializer.DefaultDecoder
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming._
@@ -21,24 +19,16 @@ import org.apache.hadoop.hbase.mapred.TableOutputFormat
 import scala.collection.mutable.ArrayBuffer 
 
 
-object ActivitySummary{
+object TransposeActivityData{
   def main(args: Array[String]){
+    val conf = new SparkConf().setAppName("TransposeActivityData")
+    val ssc = new StreamingContext(conf,Seconds(1))
 
-    val conf = new SparkConf().setAppName("ActivitySummary")
-    val ssc = new StreamingContext(conf,Seconds(5))
-
-    val kafkaParams = Map[String,String]("metadata.broker.list" -> "172.27.100.14:9092,172.27.100.15:9092,172.27.100.16:9092,172.27.100.17:9092,172.27.100.18:9092,172.27.100.19:9092,172.27.100.20:9092,172.27.100.21:9092,172.27.100.22:9092")
-    val kafkaStream = KafkaUtils.createDirectStream[String,String,StringDecoder,StringDecoder](ssc,kafkaParams,Set("raw_tracking"))
-//    val kafkaStream =KafkaUtils.createStream(ssc,"172.27.100.11:2181,172.27.100.12:2181,172.27.100.13:2181","ActivitySummary",Map("raw_tracking"->9))
-//=====バグふむパティーン
-//    val kafkaStream = KafkaUtils.createDirectStream(ssc,kafkaParams,Set("raw_tracking")).print
-
-    //parseしたデータセット
+    val kafkaStream = KafkaUtils.createStream(ssc,"172.27.100.11:2181,172.27.100.12:2181,172.27.100.13:2181","TransposeActivityData",Map("raw_tracking"->9))
+    //中間データ
     val base_data = kafkaStream
     .map(line => parseRecode(line))
     .map(fields => parseColumns(fields))
-
-    //中間データ
     val middle_data = base_data
     .flatMap(d =>{
        var open_id:String = ""
@@ -78,7 +68,6 @@ object ActivitySummary{
 
        list
     })
-//    .checkpoint(Seconds(60))
     
     //中間データからuuの重複排除
     var uu = middle_data
@@ -88,17 +77,11 @@ object ActivitySummary{
     .map(x => x.substring(0,x.lastIndexOf("_")))
     .countByValue()
 
-    //中間データからPVを数えるよ！
     val pv = middle_data
     .filter(_._1.contains("_pv_"))
-    .map(x => x._1.substring(0,x._1.lastIndexOf("_")))
+    .map(x => x._1)
+    .map(x => x.substring(0,x.lastIndexOf("_")))
     .countByValue()
-    
-    //pvの移動平均用
-    val window_pv = middle_data
-    .filter(_._1.contains("_pv_"))
-    .map(x => "window_" + x._1.substring(0,x._1.lastIndexOf("_")))
-    .countByValueAndWindow(Seconds(60),Seconds(5))
 
 
     pv
@@ -107,29 +90,50 @@ object ActivitySummary{
       val t = (Long.MaxValue-System.currentTimeMillis)
       rdd.map(x =>{ (x._1,x._2,t)})
     })
-    .map(x =>writeToActivitySummary(x))
-    .map(x=>(x._3,x._1 + "::" + x._2))
-    .groupByKey
-    .foreachRDD(rdd=>{
-      val msg:String = rdd.map(x => ("spark-time::"+x._1,x._2.mkString(",")))
-      .filter(_._2.length != 0)
-      .collect
-      .mkString(",")
-      .replaceAll("\\(","").replaceAll("\\)","")
-      writeToKafka(msg)
-      printf(msg)
-      
+    .map(x =>{
+      val hConf = HBaseConfiguration.create()
+      hConf.set("hbase.zookeeper.quorum","172.27.100.11:2181,172.27.100.12:2181,172.27.100.13:2181")
+      hConf.set("hbase.master","172.27.100.11:60000")
+      hConf.set("hbase.rootdir","/hbase")
+      val hTable = new HTable(hConf,"h_summary")
+      val row = new Put(Bytes.toBytes(x._3.toString))
+      val cF = if(x._1.contains("_uu_")) "summary_uu" else "summary_pv"
+      row.add(Bytes.toBytes(cF),Bytes.toBytes(x._1),Bytes.toBytes(x._2.toString))
+      hTable.put(row)
+      x
     })
+    .foreachRDD(rdd=>{
+      var time:String = ""
+      val msg:String = rdd.map(x =>{
+        if(time.length != 0) time = x._3.toString
+        x._1 +"::"+ x._2
+      })
+      .collect
+      .mkString(time,",","")
+      val props:Properties = new Properties()
+      props.put("metadata.broker.list","172.27.100.14:9092,172.27.100.15:9092,172.27.100.16:9092,172.27.100.17:9092,172.27.100.18:9092,172.27.100.19:9092,172.27.100.20:9092,172.27.100.21:9092,172.27.100.22:9092,172.27.100.29:9092")
+      props.put("serializer.class", "kafka.serializer.StringEncoder")
+      props.put("request.required.acks", "1")
+      val config:ProducerConfig = new ProducerConfig(props)
+      val producer:Producer[String,String] = new Producer[String,String](config)
+      val data:KeyedMessage[String,String] = new KeyedMessage[String,String]("tracking_summary",msg)
+      producer.send(data)
+      producer.close
+    })
+
+
     sys.ShutdownHookThread{
       System.out.println("Gracefully stopping ActivitySummary")
       ssc.stop(true,true)
       System.out.println("ActivitySummary stopped")
     }
 
-    ssc.checkpoint("/tmp/com/dmm/i3/summary/ActivitySummary-checkpoint")
     ssc.start()
     ssc.awaitTermination()
   }
+  //def summary(data: (String,String)):String = {
+  //   val (key,value) = data
+  //}
   def parseRecode(data: (String,String)):Array[String] = {
     val (key,v) = data
     v.split(",")
@@ -142,51 +146,5 @@ object ActivitySummary{
       (key,value.slice(1,value.size))
     })
   }
-  def writeToActivitySummary(data:(String,Long,Long)):(String,Long,Long) = {
-      val cF = if(data._1.contains("_uu_")) "summary_uu" else "summary_pv"
-      HBaseWriter.getInstance().write(data._3.toString,cF,data._1,data._2.toString)
-      data
-  }
-  def writeToKafka(msg:String) : String ={
-    if(msg.length != 0) KafkaProducer.getInstance().send("tracking_summary",msg)
-//    producer.close
-    msg
-  }
-}
-class HBaseWriter private(zkList:String,master:String){
-  val hConf = HBaseConfiguration.create()
-  hConf.set("hbase.zookeeper.quorum",zkList)
-  hConf.set("hbase.master",master)
-  hConf.set("hbase.rootdir","/hbase")
-  val hTable = new HTable(hConf,"h_summary")
-  def write(key:String,cf:String,c:String,v:String){
-    val row = new Put(Bytes.toBytes(key))
-    row.add(Bytes.toBytes(cf),Bytes.toBytes(c),Bytes.toBytes(v))
-    hTable.put(row)
-  }
-  def close(){}
-}
-object HBaseWriter{
-  private val hbWriter = new HBaseWriter("172.27.100.11:2181,172.27.100.12:2181,172.27.100.13:2181","172.27.100.11:60000")
-  def getInstance():HBaseWriter = { hbWriter }
-  def apply():HBaseWriter ={ getInstance }
-}
-class KafkaProducer private(brokerList:String){
-  val props:Properties = new Properties()
-  props.put("metadata.broker.list",brokerList)
-  props.put("serializer.class", "kafka.serializer.StringEncoder")
-  props.put("request.required.acks", "1")
-  val config:ProducerConfig = new ProducerConfig(props)
-  val producer:Producer[String,String] = new Producer[String,String](config)
-  def send(topicName:String,msg:String){
-    val data:KeyedMessage[String,String] = new KeyedMessage[String,String](topicName,msg)
-    producer.send(data)
-  }
-  def close(){}
-}
-object KafkaProducer{
-  private val kProducer = new KafkaProducer("172.27.100.14:9092,172.27.100.15:9092,172.27.100.16:9092,172.27.100.17:9092,172.27.100.18:9092,172.27.100.19:9092,172.27.100.20:9092,172.27.100.21:9092,172.27.100.22:9092,172.27.100.29:9092")
-  def getInstance():KafkaProducer ={ kProducer }
-  def apply():KafkaProducer ={ getInstance }
 }
 
